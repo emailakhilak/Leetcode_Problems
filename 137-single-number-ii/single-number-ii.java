549d3f3b2781594d9e51467cef0298de1e830f79
emailakhilak <email.akhila.k@gmail.com>
2025-10-25 10:53:05 +0530

    Time: 240 ms (5.85%) | Memory: 45.5 MB (68.80%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+     int result=0;
+     for(int i=0;i<nums.length;i++){
+        int count=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]==nums[j])
+            count++;
+        }
+        if(count!=3)
+        result=nums[i];
+     }
+     return result; 
+    }
+}
