799610611601e12d4eab6befb601c8055b28e95a
emailakhilak <email.akhila.k@gmail.com>
2026-03-09 22:23:01 +0530

    Time: 2 ms (19.40%) | Memory: 46.9 MB (44.39%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        String[] arr=new String[n];
+        for(int i=1;i<=arr.length;i++){
+            if(i%3==0 && i%5==0)
+            arr[i-1]="FizzBuzz";
+            else if(i%3==0)
+            arr[i-1]="Fizz";
+            else if(i%5==0)
+            arr[i-1]="Buzz";
+            else
+            arr[i-1]=""+i;
+        }
+        return Arrays.asList(arr);
+        
+    }
+}
