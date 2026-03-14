5dc5240f3fa46d7c5f71f28c51e314938802874a
emailakhilak <email.akhila.k@gmail.com>
2026-03-14 09:58:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (63.56%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       // List<Integer> list=new ArrayList<>();
+       int result=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+            //list.add(i,target);
+            result=i;
+            break;
+            }
+            else if(nums[i]<target)
+            result=nums.length;
+        }
+        return result;
+    }
+}
