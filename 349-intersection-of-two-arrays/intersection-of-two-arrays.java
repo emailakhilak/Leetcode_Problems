d4b6d30bc94e0aaaf6d88131b97053080b39cbf0
emailakhilak <email.akhila.k@gmail.com>
2026-03-17 21:54:36 +0530

    Time: 5 ms (31.41%) | Memory: 45.1 MB (52.37%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+      Set<Integer> set1=new HashSet<>();
+      Set<Integer> set2=new HashSet<>();
+      for(int i=0;i<nums1.length;i++){
+        set1.add(nums1[i]);
+      }
+      for(int i=0;i<nums2.length;i++){
+        if(set1.contains(nums2[i]))
+        set2.add(nums2[i]);      
+      }
+      int[] arr = set2.stream().mapToInt(Integer::intValue).toArray();
+      return arr;
+    
+
+    }
+}
